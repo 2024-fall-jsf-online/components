5e32bcdfc09a2deae1bc60fb362c9300b2a7aa86
Hendrix Gullixson <hgullixson@gmail.com>
2024-10-10 15:33:49 -0500

    Generated additional comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,12 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { TjsAdditionComponent } from './tjs-addition/tjs-addition.component';
 
 @NgModule({
   declarations: [
     AppComponent
-    , PizzaToppingsComponent
+    , PizzaToppingsComponent, TjsAdditionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tjs-addition/tjs-addition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tjs-addition',
+  templateUrl: './tjs-addition.component.html',
+  styleUrl: './tjs-addition.component.css'
+})
+export class TjsAdditionComponent {
+
+}
